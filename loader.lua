082bd265f76c9d9ab1b74d08109e3c9b8c0145cb
sample-hit <sample.hit@atomicmail.io>
2026-01-02 19:17:47 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
-loadstring(game:HttpGet('https://api.getpolsec.com/scripts/hosted/343807ce8ba9db5866deeb12d4f235612d7a72e7119276bdcb63896effe69846.lua'))();
+_G.Execs = {"Potassium","Strikes", "Chocosploit", "Volt", "Seliware", "Volcano", "Macsploit", "Hydrogen"}
+_G.Name = identifyexecutor()
+_G.meowmeow = function()
+    for _, v in next, _G.Execs do
+        if _G.Name:find(v) then 
+            loadstring(game:HttpGet('https://api.getpolsec.com/scripts/hosted/343807ce8ba9db5866deeb12d4f235612d7a72e7119276bdcb63896effe69846.lua'))();
+            return true 
+        end
+    end
+    warn('executor_unsupported')
+end
+_G.meowmeow()
